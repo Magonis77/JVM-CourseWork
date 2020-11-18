8cbd226ada4e86cff589f6ca398ba197ad4dc031
Daniels Magonis <44064884+Magonis77@users.noreply.github.com>
2020-11-18 16:09:49 +0000

    difference betweeen start and end dates included code

---
+++ src/ManageTeam.kt
@@ -0,0 +1,40 @@
+import java.util.*
+
+data class ManageTeam(var name: String = "Team", var members: Array<String>) {
+
+}
+data class members(var members: Array<String>){
+
+}
+data class teamname(var teamname: String){
+
+}
+//val liss: DefaultListModel<*> = DefaultListModel<Any?>()
+class ManageHandler() {
+    fun sendlist(listing: String= "Team"):members{
+        var al = ArrayList<String>()
+        var list: String = listing
+        var split = list.replace("[", ",").replace("]", "").replace(" ", "")
+        var everything = split.split(",".toRegex()).toTypedArray()
+        var test = everything[0]
+        var members = split.replace(test, "").split(",".toRegex()).toTypedArray()
+
+        return members(members)
+    }
+
+    fun getteamname(teamname: String = "Team A"):teamname{
+            var al = ArrayList<String>()
+            var list: String = teamname
+            var split = list.replace("[", ",").replace("]", "").replace(" ", "")
+            var everything = split.split(",".toRegex()).toTypedArray()
+            var test = everything[0]
+
+            return teamname(test)
+        }
+    }
+
+
+
+
+
+
